150d14fb9991b327dd5446f2dce985100c8ae570
G!bulk <gibulk@yahoo.com>
2026-04-19 11:41:01 +0700

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,13 +12,13 @@ export const createClient = async () => {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options)
             })
           } catch {
-            // Ignore
+            // Ignore error when called from Server Component
           }
         },
       },
@@ -37,13 +37,13 @@ export const createAdminClient = async () => {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
               cookieStore.set(name, value, options)
             })
           } catch {
-            // Ignore
+            // Ignore error when called from Server Component
           }
         },
       },
