2931f2405f3df1a5efdaf849afc9a33d52e34f2e
G!bulk <gibulk@yahoo.com>
2026-04-19 09:12:30 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'oyeavwuqlbhrqjkmxyly.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/article-images/**',
+      },
+    ],
+  },
+}
 
+module.exports = nextConfig
