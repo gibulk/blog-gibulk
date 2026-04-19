7b78f40a5af61ed947443ed717bdb641e4be2c94
G!bulk <gibulk@yahoo.com>
2026-04-19 10:12:59 +0700

    Update articlecontent.tsx

--- components/artikel/articlecontent.tsx
+++ components/artikel/articlecontent.tsx
@@ -1,1 +1,14 @@
+'use client'
 
+interface ArticleContentProps {
+  content: string
+}
+
+export default function ArticleContent({ content }: ArticleContentProps) {
+  return (
+    <div 
+      className="prose prose-lg dark:prose-invert max-w-none"
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  )
+}
