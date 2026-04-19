a7a84b386159eb89f9e1674ed236c05f736ef772
G!bulk <gibulk@yahoo.com>
2026-04-19 10:22:08 +0700

    Update layout.tsx

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,1 +1,14 @@
+import { Toaster } from 'react-hot-toast'
 
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <Toaster position="top-right" />
+      {children}
+    </>
+  )
+}
